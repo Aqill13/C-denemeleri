ef54e90f5895d6f20a9e599fbbe7f2f1f8847dd7
aqil <aqilkalbiyev01@gmail.com>
2024-03-09 22:40:58 +0400

    ilk commit

---
+++ Deneme.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Deneme
+    {
+        public static int row;
+        public static int col;
+
+        public static void yazdir()
+        {
+            Console.Write("Enter Row: ");
+            row = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Column: ");
+            col = int.Parse(Console.ReadLine());
+        }
+
+        public static void arrDisplay()
+        {
+            int[,] arr = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ConsoleApp3;
+
+
+namespace test
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Deneme.yazdir();
+            Deneme.arrDisplay();
+
+            Console.ReadLine();
+        }
+    }
+}
